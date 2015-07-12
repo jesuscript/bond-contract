c9ebcec91e9b48a945fd2acee23cd6f33f1a1c4d
Ken Kappler <kenneth.kappler@live.co.uk>
2015-07-12 12:44:56 +0100

    test contract

---
+++ contracts/thirteen.sol
@@ -0,0 +1,23 @@
+contract thirteen{
+
+uint public one;
+uint public two;
+uint public three;
+uint public four;
+uint public five;
+uint public six;
+	function thirteen(){
+		one = 1;
+		two = 2;
+	}
+	function seven(uint a)returns(uint a){
+		returns a;
+	}
+	function eight(){}
+
+	function nine(){}
+	function ten(){}
+	function eleven(){}
+	function twelve(){}
+
+}
